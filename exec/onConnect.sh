09d02845ccb1870689e2de0d65eaeb3cb1e1a13b
ebarault <eric.barault@gmail.com>
2017-09-12 01:00:45 +0200

    sample exec script

---
+++ exec/onConnect.sh
@@ -0,0 +1,16 @@
+function onConnectedUser() {
+  echo "User $1 successfully connected" >> /var/log/proftpd/exec_onConnect.log
+}
+
+while test $# -gt 0; do
+  case "$1" in
+    -u|--user)
+      shift
+      onConnectedUser $1
+      exit 0
+      ;;
+     *)
+      shift
+      ;;
+  esac
+done
