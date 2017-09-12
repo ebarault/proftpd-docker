4311343533864ac1a8912b603a8be502054c58cd
ebarault <eric.barault@gmail.com>
2017-09-12 17:59:37 +0200

    new sql migration, new sql commands to update last_access and login_history

--- sql/proftp_tables.sql
+++ sql/proftp_tables.sql
@@ -1,32 +1,29 @@
 CREATE SCHEMA ftp;
-GRANT USAGE ON SCHEMA ftp TO xydatadev_user;
+GRANT USAGE ON SCHEMA ftp TO db_user;
 
 CREATE TABLE ftp.users (
-  pkid      serial      PRIMARY KEY,
-  userid    text        NOT NULL UNIQUE,
-  passwd    text,
-  uid       int,
-  gid       int,
-  homedir   text,
-  shell     text
+  userid    VARCHAR(30) NOT NULL UNIQUE,
+  passwd    VARCHAR(80) NOT NULL,
+  uid       INTEGER UNIQUE,
+  gid       INTEGER,
+  homedir   VARCHAR(255),
+  shell     VARCHAR(255),
+  last_accessed TIMESTAMP WITH TIME ZONE
 );
-GRANT SELECT ON ftp.users TO xydatadev_user;
+GRANT SELECT, UPDATE ON ftp.users TO db_user;
 
-CREATE TABLE groups (
-        groupname text NOT NULL,
-        gid integer,
-        members text
+CREATE TABLE ftp.groups (
+  groupname VARCHAR(30) NOT NULL,
+  gid INTEGER NOT NULL,
+  members VARCHAR(255)
 );
-GRANT SELECT ON ftp.groups TO xydatadev_user;
+GRANT SELECT ON ftp.groups TO db_user;
 
-CREATE TABLE ftp.file_log (
-  pkid               serial      PRIMARY KEY,
-  userid             text        REFERENCES ftp.users(userid),
-  abs_path           text,
-  file               text,
-  dns                text,
-  time_transaction   text,
-  ts_in              timestamp with time zone   NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE ftp.login_history (
+  userid VARCHAR NOT NULL,
+  client_ip VARCHAR NOT NULL,
+  server_ip VARCHAR NOT NULL,
+  protocol VARCHAR NOT NULL,
+  login_date TIMESTAMP WITH TIME ZONE
 );
-GRANT INSERT ON ftp.file_log TO xydatadev_user;
-GRANT UPDATE ON TABLE ftp.file_log_pkid_seq TO xydatadev_user;
+GRANT INSERT ON ftp.login_history TO db_user;
