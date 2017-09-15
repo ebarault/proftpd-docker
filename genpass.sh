a21d55a94677f6c9117029c923a7071d46e1e6e7
ebarault <eric.barault@gmail.com>
2017-09-15 11:31:42 +0200

    adding genpass script

---
+++ genpass.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+package="./genpass.sh"
+
+function help {
+  echo "$package - generate passwords for proftpd server"
+  echo " "
+  echo "password formula is: [ SHA256( SALT + password ) ].hex64"
+  echo " "
+  echo "usage:"
+  echo "$package -s salt/.sh password"
+  echo "$package --salt salt/.sh password"
+}
+
+
+function generatePassword() {
+  { echo -n $1; echo -n `cat $SALT_FILE`; } | openssl dgst -binary -sha256 | openssl enc -base64 -A
+}
+
+if [ "${1}" != '-s' ] && [ "${1}" != '-h' ]; then
+  echo " "
+  echo "Please specify the path to the salt file"
+  echo " "
+  echo "Example : $package -s salt/.sh password"
+  echo " "
+  exit -1
+else
+  if [ "${1}" = '-h' ]; then
+    help
+    exit 0
+  fi
+  shift
+  SALT_FILE=$1
+  shift
+  generatePassword $1
+fi
